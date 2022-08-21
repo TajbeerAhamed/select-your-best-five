acd2cfc04b3078fd13601cf575dbaaa01d6d0d47
TajbeerAhamed <mdtajbeerahamedrimon@gmail.com>
2022-08-22 00:30:35 +0600

    some-js-added

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const selectNeymar = document.getElementById('select-neymar').addEventListener('click', function () {
+    const neymar = document.getElementById('neymar')
+    const neymarName = neymar.innerText
+    const playerList = document.getElementById('player-list')
+    const li = document.createElement('li')
+    li.innerText = neymarName
+
+
+
+})
+
+
+
